65ff3091142ad0942fd33d71db1a679abb5ad46e
developerjunior5 <godfreynyatoti7@gmail.com>
2024-06-15 15:18:52 +0200

    implement full website

---
+++ script.js
@@ -0,0 +1,26 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal("#header-container h3", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal("#header-container p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal("#about  p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal("#btn-appointment", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal("#services", {
+  ...scrollRevealOption,
+  interval: 1500,
+});
