98b39e0752ca55f9ae5d8f2bd63d42a2d210d952
developerjunior5 <godfreynyatoti7@gmail.com>
2024-06-27 21:12:25 +0200

    implemt full navbar navigation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const openMenu = document.querySelector(".open__menu");
+const closeMenu = document.querySelector(".close__menu");
+navLinks = document.querySelector(".nav__links");
+mobileNavLinks = document.querySelector(".mobile__nav--links");
 const scrollRevealOption = {
   distance: "50px",
   origin: "bottom",
@@ -23,4 +27,37 @@ ScrollReveal().reveal("#btn-appointment", {
 ScrollReveal().reveal("#services", {
   ...scrollRevealOption,
   interval: 500,
+});
+
+//script for the navigation
+openMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  mobileNavLinks.classList.remove("right-[-100%]");
+  mobileNavLinks.classList.add("right-0");
+});
+
+closeMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  mobileNavLinks.classList.add("right-[-100%]");
+  mobileNavLinks.classList.remove("right-0");
+});
+
+navLinks.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+mobileNavLinks.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("mobile__nav--link")) {
+    const id = e.target.getAttribute("href");
+    mobileNavLinks.classList.add("right-[-100%]");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-primary": "#1434A4",
+        "orange-secondary": "#FF5733",
+      },
+      fontFamily: {
+        Inter: ["Inter", ...defaultTheme.fontFamily.sans],
+        Dosis: ["Dosis", ...defaultTheme.fontFamily.sans],
+        Port: ["Port Lligat Sans", ...defaultTheme.fontFamily.sans],
+        Inria: ["Inria", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
